f398da4c072c8eacbf7afed8385f707bb1f358be
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2022-12-07 00:03:00 +0700

    hello-world: finish

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,11 +2,38 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	engHelloPrefix     = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := greetingPrefix(language)
+
+	return prefix + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = engHelloPrefix
+	}
+
+	return
 }
 
 func main() {
 	x := "Huy"
-	fmt.Println(Hello(x))
+	fmt.Println(Hello(x, ""))
 }
