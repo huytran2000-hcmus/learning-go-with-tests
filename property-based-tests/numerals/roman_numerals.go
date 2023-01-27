21074f4274b6224ac5e0b70f93f7d3c8c6cac3fb
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-01-27 15:41:28 +0700

    property-based-tests: Property test convert to roman and back

--- property-based-tests/numerals/roman_numerals.go
+++ property-based-tests/numerals/roman_numerals.go
@@ -1,15 +1,20 @@
 package numerals
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type romanNumeral struct {
-	value  int
+	value  uint16
 	symbol string
 }
 
+const MaximumRomanNumeralVal uint16 = 3999
+
 type romanNumerals []romanNumeral
 
-func (r *romanNumerals) ValueOf(symbols ...byte) int {
+func (r *romanNumerals) ValueOf(symbols ...byte) uint16 {
 	symbol := string(symbols)
 
 	for _, s := range allRomanNumerals {
@@ -37,7 +42,10 @@ var allRomanNumerals = romanNumerals{
 	{1, "I"},
 }
 
-func ConvertToRoman(arabic int) string {
+func ConvertToRoman(arabic uint16) (string, error) {
+	if arabic > 3999 {
+		return "", fmt.Errorf("%d is above the roman numeral maximum value", arabic)
+	}
 	var result strings.Builder
 
 	for _, numeral := range allRomanNumerals {
@@ -47,17 +55,17 @@ func ConvertToRoman(arabic int) string {
 		}
 	}
 
-	return result.String()
+	return result.String(), nil
 }
 
-func ConvertToArabic(roman string) int {
-	var total int
+func ConvertToArabic(roman string) uint16 {
+	var total uint16
 	var i int
 	for i = 0; i < len(roman)-1; i++ {
 		symbol := roman[i]
 		nextSymbol := roman[i+1]
 
-		// Look ahead to see if can combine symbols
+		// Look ahead to see if it is possible to combine symbols
 		potentialValue := allRomanNumerals.ValueOf(symbol, nextSymbol)
 		if potentialValue != 0 {
 			total += potentialValue
--- property-based-tests/numerals/roman_numerals_test.go
+++ property-based-tests/numerals/roman_numerals_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 var tests = []struct {
-	arabic int
+	arabic uint16
 	roman  string
 }{
 	{1, "I"},
@@ -40,9 +40,11 @@ func TestConvertToRoman(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%d gets converted to %s", tt.arabic, tt.roman), func(t *testing.T) {
 			t.Parallel()
+			got, _ := ConvertToRoman(tt.arabic)
+			want := tt.roman
 
-			if got := ConvertToRoman(tt.arabic); got != tt.roman {
-				t.Errorf("ConvertToRoman(%d) = %q, want %q", tt.arabic, got, tt.roman)
+			if got != want {
+				t.Errorf("ConvertToRoman(%d) = %q, want %q", tt.arabic, got, want)
 			}
 		})
 	}
@@ -59,4 +61,45 @@ func TestConvertToArabic(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestConvertInvalidArabicToRoman(t *testing.T) {
+	for i := 1; i <= 10; i++ {
+		arabic := MaximumRomanNumeralVal + uint16(i*i)
+		t.Run(fmt.Sprintf("%d convert roman numeral return error", arabic), func(t *testing.T) {
+			_, err := ConvertToRoman(arabic)
+
+			if err == nil {
+				t.Errorf("%d can't be convert to roman but this didn't cause an error", arabic)
+			}
+		})
+	}
+}
+
+func FuzzConvertToRomanAndBackProperty(f *testing.F) {
+	for _, tt := range tests {
+		f.Add(tt.arabic)
+	}
+	f.Add(uint16(3999))
+	f.Add(uint16(10000))
+
+	f.Fuzz(func(t *testing.T, arabic uint16) {
+		roman, err := ConvertToRoman(arabic)
+		if err != nil {
+			t.Skip("Invalid number to convert to roman")
+		}
+
+		fromRoman := ConvertToArabic(roman)
+
+		if fromRoman != arabic {
+			t.Errorf(
+				"ConvertToRoman(%d) = %q, ConvertToArabic(%q) = %d, want %d",
+				arabic,
+				roman,
+				roman,
+				fromRoman,
+				arabic,
+			)
+		}
+	})
 }
